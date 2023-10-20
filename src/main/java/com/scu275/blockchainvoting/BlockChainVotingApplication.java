78999ab509eba86f7e2d523c537c6538787d9129
Yunchen Zhang <yzhang39@scu.edu>
2023-10-20 15:37:44 -0700

    first init

---
+++ src/main/java/com/scu275/blockchainvoting/BlockChainVotingApplication.java
@@ -0,0 +1,13 @@
+package com.scu275.blockchainvoting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlockChainVotingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlockChainVotingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/scu275/blockchainvoting/BlockChainVotingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scu275.blockchainvoting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlockChainVotingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
