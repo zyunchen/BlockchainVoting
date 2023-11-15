cb3bdfb4b6918bbb56843962d0aff2e563b30d1e
Yunchen Zhang <yzhang39@scu.edu>
2023-11-14 23:58:25 -0800

    fix bug

--- src/main/java/com/scu275/invoicemanagement/entity/InvoiceRepository.java
+++ src/main/java/com/scu275/invoicemanagement/entity/InvoiceRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Date;
 import java.util.List;
@@ -12,6 +13,7 @@ public interface InvoiceRepository extends JpaRepository<Invoice, Long> {
     public List<Invoice> findByCreateUser_uId(long uid);
 
     @Modifying
+    @Transactional
     @Query("UPDATE Invoice i SET i.status = :status WHERE i.invoiceId = :invoiceId")
     void updateStatusByInvoiceId(@Param("invoiceId") Long invoiceId, @Param("status") String status);
 
